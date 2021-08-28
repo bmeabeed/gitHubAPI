7b89bbb51cb3a4ea04eeb86cbb5e96a9eb6526c0
bayomi <bmeabeed@gmail.com>
2021-08-28 11:32:52 +0200

    Create Initial Spring Boot Project

---
+++ gitHupRepos/src/main/java/com/gitHup/listRepo/GitHupReposApplication.java
@@ -0,0 +1,13 @@
+package com.gitHup.listRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHupReposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHupReposApplication.class, args);
+	}
+
+}
---
+++ gitHupRepos/src/test/java/com/gitHup/listRepo/GitHupReposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitHup.listRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHupReposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
